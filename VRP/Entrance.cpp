56fd7bdf6080b748604d2e272439ba52896921fd
lvning04 <lvning04@meituan.com>
2021-10-26 11:11:47 +0800

    10/26

--- VRP/Entrance.cpp
+++ VRP/Entrance.cpp
@@ -1,3 +1,8 @@
+#define GOOGLE_GLOG_DLL_DECL
+#define GLOG_NO_ABBREVIATED_SEVERITIES
+
+#include <glog/logging.h>
+
 #include <iostream>
 #include <string>
 #include <tinyxml.h>
@@ -6,6 +11,7 @@
 #include "ALNS/BasicClass/ISolutionNode.h"
 #include "Configuration.h"
 #include "FerryVehicleTask.h"
+#include "Header.h"
 #include "IInformation.h"
 
 
@@ -24,6 +30,17 @@ void ALNS_Entrance();
 void qt_environment();
 
 int main() {
+    google::InitGoogleLogging("VRP");
+    FLAGS_alsologtostderr = true;
+    FLAGS_colorlogtostderr = true;
+
+    google::SetLogDestination(google::GLOG_INFO, "D:\\学习资料\\研究进度\\研二\\数据分析\\LOG\\INFO_");
+    google::SetLogDestination(google::GLOG_WARNING, "D:\\学习资料\\研究进度\\研二\\数据分析\\LOG\\WARNING_");
+    google::SetLogDestination(google::GLOG_ERROR, "D:\\学习资料\\研究进度\\研二\\数据分析\\LOG\\ERROR_");
+    google::SetLogDestination(google::GLOG_FATAL, "D:\\学习资料\\研究进度\\研二\\数据分析\\LOG\\FATAL_");
+
+    LOG(INFO) << "Welcome to use the Vehicle Routing Problem procedure!";
+
     welcomeInterface();
     Configuration configuration = loadConfiguration();
    
@@ -40,5 +57,5 @@ void qt_environment() {
 
 void welcomeInterface()
 {
-    cout << "Welcome to use the Vehicle Routing Problem procedure!" << endl;
+   
 }
---
+++ VRP/Header.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#define GOOGLE_GLOG_DLL_DECL
+#define GLOG_NO_ABBREVIATED_SEVERITIES
+
+#include <glog/logging.h>
+class Header
+{
+};
+
